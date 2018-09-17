9bd25a0d8c1ec9837660df1974fe87756158444c
Adam Iwaniuk <aiwaniuk@sii.pl>
2018-09-17 11:55:24 +0200

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 
 socket.on('connect', function()  {
     console.log('connected to server');
-    socket.emit('createMessage', {
-        from: 'test@test.com,',
-        text: 'Hey, this is test.'
-    });
 });
 socket.on('disconnect', function() {
     console.log('Disconnected from server.');
